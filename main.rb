f47440a86f58e4ddadc65db9215a7f0e08e93713
rixede <rixede@gmail.com>
2018-05-22 11:39:59 -0400

    ajout de main.rb apres clone initial
    Pour pas que codeanywhere ne cree un project ruby-on-rails.

---
+++ main.rb
@@ -0,0 +1,13 @@
+# Coucou Gabriel!
+# Normalement j'ai tout configuré et tu devrais pouvoir entrer ton code ici,
+# et l'exécuter en cliquant sur le triangle dans la barre de menu (avant dernier
+# icone avant l'engrenage).
+# Essayes maintenant, et ça devrait ouvrir une autre fenêtre qui affichera la
+# "sortie" de ce programme.
+
+puts 'hello'
+
+# Lorsque ceci fonctionnera, tu pourras réfléchire à ta première mission.
+# Ta première mission est de créer un nouveau fichier "card.rb" à côté de ce fichier
+# "main.rb". Et lorsque tu auras fait ça (n'oublis pas de tout bien sauvegarder),
+# envoie moi un petit message sur Hangouts.
